b9914d493ce2c0694f5b0b01997113b14fd618e4
김세미 <semi@gimsemiui-MacBookAir.local>
2026-04-07 16:53:05 +0900

    최최종 수정

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="card">
       {/* 아래 텍스트들을 자유롭게 수정해보세요 */}
       <h1>안녕하세요 짝짝!👋</h1>
-      <p>저는 세미입니다! 코딩을 배우고 ㅇㅇ 있어요.너무 신나! </p>
+      <p>저는 세미에요. 두 번째 수정하고 있어요 :) </p>
       <p>이 페이지의 글자를 직접 바꿀꺼야!</p>
       <span className="tag">AI PT 연습 중</span>
     </div>
